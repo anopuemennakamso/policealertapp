dbb91fc227f0e1d845068478a6098a3818d52104
anopuemennakamso <158099777+anopuemennakamso@users.noreply.github.com>
2026-09-22 23:09:59 +0100

    Change CORS middleware to disallow credentials

--- sos_backend/main.py
+++ sos_backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="SOS Emergency Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )

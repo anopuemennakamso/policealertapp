2ee33d8c26a0890fdbb534c6a0a43dedfa67f1ae
anopuemennakamso <158099777+anopuemennakamso@users.noreply.github.com>
2026-09-22 23:43:08 +0100

    Update main.py

--- sos_backend/main.py
+++ sos_backend/main.py
@@ -12,7 +12,7 @@ from database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SOS Emergency Backend")
-    app.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
